84468f07b6498a68ef9dc99ff9f645e5b109587b
alach2 <alach@dons.usfca.edu>
2023-11-06 21:10:26 -0800

    Create Elevator.java

---
+++ Elevator.java
@@ -0,0 +1,20 @@
+import java.util.Properties;
+import java.util.FileInputStream;
+import java.util.*;
+
+public class Passenger{
+
+}
+
+public class Floor{
+
+}
+public class Elevator {
+    
+}
+
+public class Main{
+    public static void main(String[] args) throws Exception{
+
+    }
+}
